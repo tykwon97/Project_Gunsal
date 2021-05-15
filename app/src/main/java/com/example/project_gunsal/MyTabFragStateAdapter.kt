006f5ebca89ffb78abdc791b069d837d1591bbb9
LeeMinHo <alsghsla1227@gmail.com>
2021-05-15 21:58:18 +0900

    all of xml, fragment file crete

---
+++ app/src/main/java/com/example/project_gunsal/MyTabFragStateAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.project_gunsal
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.project_gunsal.fragment.*
+
+class MyTabFragStateAdapter (fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int {
+        return 5
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0 -> Today_Viewer_Fragment()
+            1 -> Monthly_Statistics_Fragment()
+            2 -> Health_News_Fragment()
+            3 -> Stretching_Player_Fragment()
+            4 -> Setting_Fragment()
+            else -> Today_Viewer_Fragment()
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/project_gunsal/MyViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.project_gunsal
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyViewModel : ViewModel() {
+    val selectednum = MutableLiveData<Int>()
+    fun setLiveData(num: Int) {
+        selectednum.value = num
+    }
+}
