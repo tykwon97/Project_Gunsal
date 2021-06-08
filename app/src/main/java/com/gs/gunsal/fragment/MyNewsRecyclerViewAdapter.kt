a85908477d454990735a94bf97cb84dfd5477591
MinHo <my_alsgh@naver.com>
2021-06-08 13:33:30 +0900

    Update MyNewsRecyclerViewAdapter.kt

--- app/src/main/java/com/gs/gunsal/fragment/MyNewsRecyclerViewAdapter.kt
+++ app/src/main/java/com/gs/gunsal/fragment/MyNewsRecyclerViewAdapter.kt
@@ -3,14 +3,32 @@ package com.gs.gunsal.fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.text.parseAsHtml
 import androidx.recyclerview.widget.RecyclerView
+import com.gs.gunsal.R
 import com.gs.gunsal.databinding.FragmentHealthNewsBinding
 
 class MyNewsRecyclerViewAdapter(
     private val values: List<String>
 ) : RecyclerView.Adapter<MyNewsRecyclerViewAdapter.ViewHolder>() {
-
+    var image = listOf<Int>(
+        R.drawable.health_1,
+        R.drawable.health_2,
+        R.drawable.health_3,
+        R.drawable.health_4,
+        R.drawable.health_5,
+        R.drawable.health_6,
+        R.drawable.health_7,
+        R.drawable.health_8,
+        R.drawable.health_9,
+        R.drawable.health_10,
+        R.drawable.health_11,
+        R.drawable.health_12,
+        R.drawable.health_13,
+        R.drawable.health_14
+    )
     var itemOnClickListener:OnItemClickListener?=null
 
 
@@ -32,8 +50,9 @@ class MyNewsRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        holder.title.text = item
 
+        holder.title.text = item.parseAsHtml()
+        holder.image.setImageResource(image[position%14])
     }
 
     override fun getItemCount(): Int = values.size
@@ -41,6 +60,7 @@ class MyNewsRecyclerViewAdapter(
     inner class ViewHolder(binding: FragmentHealthNewsBinding) :
         RecyclerView.ViewHolder(binding.root) {
         val title: TextView = binding.newsTitle
+        val image:ImageView = binding.newsImage
         init {
             binding.click.setOnClickListener {
                 itemOnClickListener?.OnItemClick(this, it, values[adapterPosition], adapterPosition)
